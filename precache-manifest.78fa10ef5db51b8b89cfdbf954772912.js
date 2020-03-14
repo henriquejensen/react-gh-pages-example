ac846b5ddd8e454002d74e3b56fc4f3982463bf4
Henrique Jensen <henriquejensen@hotmail.com>
2020-03-14 00:55:52 -0300

    Updates

---
+++ precache-manifest.78fa10ef5db51b8b89cfdbf954772912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cb3c99a240ce7958871a3640244032",
+    "url": "/react-gh-pages-example/index.html"
+  },
+  {
+    "revision": "3e88531e6e3226c1e482",
+    "url": "/react-gh-pages-example/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "6383958b39827ec5db16",
+    "url": "/react-gh-pages-example/static/js/2.de605472.chunk.js"
+  },
+  {
+    "revision": "3e88531e6e3226c1e482",
+    "url": "/react-gh-pages-example/static/js/main.361527f4.chunk.js"
+  },
+  {
+    "revision": "b6415527e31eff92e064",
+    "url": "/react-gh-pages-example/static/js/runtime~main.be613cba.js"
+  }
+]);
